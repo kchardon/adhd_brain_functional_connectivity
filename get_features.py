a883bf4a928f895e6ef18e2144dc6262aaa8ea51
Katia <katiachardon@outlook.fr>
2023-08-07 14:05:41 +0200

    get features names

--- get_features.py
+++ get_features.py
@@ -1,17 +1,22 @@
 import numpy as np
 
 
-def get_features_name(features: dict, sensors: list):
+def get_features_name(features: dict, sensors: list, frequency_bands: dict):
     n_sensors = len(sensors)
     indices = np.tril_indices_from(np.zeros((n_sensors, n_sensors)), -1)
 
-    features_name = {}
+    features_band = {}
 
-    first_sensor = indices[0][features]
-    second_sensor = indices[1][features]
+    for band in frequency_bands:
+        features_name = {}
 
-    for i in range(len(first_sensor)):
-        features_name[i] = (sensors[first_sensor[i]],
-                            sensors[second_sensor[i]])
+        first_sensor = indices[0][features]
+        second_sensor = indices[1][features]
 
-    return features_name
+        for i in range(len(first_sensor)):
+            features_name[i] = (sensors[first_sensor[i]],
+                                sensors[second_sensor[i]])
+
+        features_band[band] = features_name
+
+    return features_band
