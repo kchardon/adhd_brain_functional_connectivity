89a430915dac938a933c83012e133a8a301feb16
Katia <katiachardon@outlook.fr>
2023-08-07 13:40:19 +0200

    get features name

---
+++ get_features.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def get_features_name(features: dict, sensors: list):
+    n_sensors = len(sensors)
+    indices = np.tril_indices_from(np.zeros((n_sensors, n_sensors)))
+
+    features_name = {}
+
+    first_sensor = indices[0][features]
+    second_sensor = indices[1][features]
+
+    for i in range(len(first_sensor)):
+        features_name[i] = (sensors[first_sensor[i]],
+                            sensors[second_sensor[i]])
+
+    return features_name
