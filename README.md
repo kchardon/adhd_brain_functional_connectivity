0425c279fdd56c7ca351844f39117611c0765e08
Katia <katiachardon@outlook.fr>
2023-08-09 15:54:25 +0200

    tests

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ADHD Brain Functional Connectivity
+# ADHD Brain Functional Connectivity with Omega
+
+During my internship, I worked on brain age prediction \[\] with the Omega dataset \[[1](brain-age1), [2](brain-age2)\]. 
+
+
+
+## References
+
+[1] {#brain-age1} Denis A. Engemann, Apolline Mellot, Richard Höchenberger, Hubert Banville, David Sabbagh, Lukas Gemein, Tonio Ball, Alexandre Gramfort,
+A reusable benchmark of brain-age prediction from M/EEG resting-state signals,
+NeuroImage,
+Volume 262,
+2022,
+119521,
+ISSN 1053-8119,
+[https://doi.org/10.1016/j.neuroimage.2022.119521](https://doi.org/10.1016/j.neuroimage.2022.119521).
+
+[2] {#brain-age2} 
--- compute_coh.py
+++ compute_coh.py
@@ -1,20 +1,31 @@
 import numpy as np
 import mne
 
-n_sensors = 262
 
-
-def compute_connectivity(epoch: mne.Epochs, fmin: float, fmax: float):
+def compute_connectivity(epoch: mne.Epochs, fmin: float, fmax: float,
+                         n_sensors: int):
+    print(len(epoch.info['ch_names']))
+    print(epoch.info['ch_names'])
+    print(len(set(epoch.info['ch_names'])))
+    print(epoch.info)
+    print(epoch)
     csd = mne.time_frequency.csd_multitaper(epoch, fmin=fmin, fmax=fmax,
-                                            n_jobs=40, verbose=False)
+                                            n_jobs=40, verbose=False,
+                                            picks=epoch.info['ch_names'])
+    print(csd)
     freqs = csd.frequencies
-    psd = epoch.compute_psd(fmin=fmin, fmax=fmax, n_jobs=40, verbose=False)
+    psd = epoch.compute_psd(fmin=fmin, fmax=fmax, n_jobs=40, verbose=False,
+                            picks=epoch.info['ch_names'])
     psd_data = psd.get_data().sum(axis=0)
+    print(psd_data)
+    print(psd_data.shape)
 
     coh = np.zeros((n_sensors, n_sensors))
+    print(coh.shape)
 
     for idx, f in enumerate(freqs):
         csd_f = np.abs(csd.get_data(f))**2
+        print(csd_f.shape)
         for i in range(n_sensors):
             for j in range(n_sensors):
                 coh[i, j] = coh[i, j] + csd_f[i, j] / (psd_data[i, idx]
